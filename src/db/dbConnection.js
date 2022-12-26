0527f949342e952323273c015726ae534d22ea32
MyktybekOmur <53742224+MyktybekOmur@users.noreply.github.com>
2022-12-26 08:11:48 +0300

    discarrt db change

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,17 +1,12 @@
-// const mongoose = require("mongoose")
-const { MongoClient } = require('mongodb');
-const client = new MongoClient(process.env.DB_URL);
-// client.connect(process.env.DB_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-//     .then(() => {
-//         console.log("Veritabanına Başarıyla Bağlandı");
-//     })
-//     .catch((err) => {
-//         console.log("Veritabanına bağlanırken hata çıktı : ", err);
-//     })
-    client.connect(process.env.DB_URL,(err)=>{
-        if(err){ console.error(err); return false;}
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("Veritabanına Başarıyla Bağlandı");
     })
-        
+    .catch((err) => {
+        console.log("Veritabanına bağlanırken hata çıktı : ", err);
+    })
